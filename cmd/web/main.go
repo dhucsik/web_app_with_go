c8a2690535180e0694778684262abcd9df057fc1
dias <diasgalikhanov@gmail.com>
2022-05-04 18:27:39 +0600

    refactoring

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/dhucsik/web_app_with_go/pkg/handlers"
 )
 
 const portNumber = ":8080"
 
 //main is the main application function
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Printf("Starting application on port %s", portNumber)
 	log.Fatal(http.ListenAndServe(portNumber, nil))
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -2,14 +2,16 @@ package handlers
 
 import (
 	"net/http"
+
+	"github.com/dhucsik/web_app_with_go/pkg/render"
 )
 
 //Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "home.page.html")
+	render.RenderTemplate(w, "home.page.html")
 }
 
 //About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "about.page.html")
+	render.RenderTemplate(w, "about.page.html")
 }
